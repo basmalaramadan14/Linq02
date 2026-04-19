b73962100a1ddbdb5c788a86d84160243582780b
Basmala Ramadan <basmalaramadan51@gmail.com>
2026-04-20 01:46:26 +0200

    Add project files.

---
+++ Linq02/Helpers/StringCaseInsensitiveComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq02.Helpers
+{
+    public class StringCaseInsensitiveComparer : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            return string.Compare(x, y, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+
+}
---
+++ Linq02/Program.cs
@@ -0,0 +1,51 @@
+﻿using Linq02.DataSources;
+
+namespace Linq02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q01
+            var result = Source.ProductList
+    .OrderByDescending(p => p.UnitPrice)
+    .Take(3);
+
+            foreach (var p in result)
+            {
+                Console.WriteLine($"{p.ProductName} - {p.UnitPrice}");
+            }
+            #endregion
+
+
+            #region Q02
+            #endregion
+
+            #region Q03
+
+            #endregion
+
+            #region Q04
+            #endregion
+
+            #region Q05
+            #endregion
+            #region Q06
+            #endregion
+            #region Q07
+            #endregion
+            #region Q08
+            #endregion
+            #region Q09
+            #endregion
+            #region Q10
+            #endregion
+            #region Q11
+            #endregion
+            #region Q12
+            #endregion
+            #region Q13
+            #endregion
+        }
+    }
+}
